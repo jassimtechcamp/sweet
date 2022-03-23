36c8a23532a194944bb95f648e299264394a7859
jassimtechcamp <101932978+jassimtechcamp@users.noreply.github.com>
2022-03-23 14:44:15 +0300

    Update py1.py

--- py1.py
+++ py1.py
@@ -2,7 +2,10 @@
 def sum(a, b):
     return (a + b)
 
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+a = int(input('ادخل الرقم الاول: '))
+b = int(input('ادخل الرقم الثاني : '))
+s1=(a*b)
+s2=(a/b)
+print(f'نتيجة جمع  {a} و {b} هي {sum(a, b)}')
+print ("ونتيجة ضرب العددين هي ",(s1))
+print(f'قسمة العدد الاول   {a} على العدد الثاني {b} هي ', (s2))
