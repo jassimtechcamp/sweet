74baca49d4573da94abda99f4423436733f4b94f
jassimtechcamp <101932978+jassimtechcamp@users.noreply.github.com>
2022-03-23 14:31:21 +0300

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,6 +1,8 @@
 
-print("اله حاسبة لجمع رقمين")
-s1=input("ادخل الرقم الاول ")
-s2=input("ادخل الرقم الثاني ")
-s3 =int (int (s1)+int (s2))
-print(s3)
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
